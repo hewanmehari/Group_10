295f0c9ef878093e10dddaffad576b21fb7359ca
hewan <hewaanmehari@gmail.com>
2025-05-27 14:55:54 +0300

    deleted two files outside the given folders

--- palindrome.js
+++
@@ -1,7 +0,0 @@
-function isPalindrome(str) {
-    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
-    const reversed = cleanStr.split('').reverse().join('');
-    return cleanStr === reversed;
-}
-console.log(isPalindrome('MaDam')); 
-console.log(isPalindrome("M@%aDaM#$")); 
